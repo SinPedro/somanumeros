0ede7da48541c574b49ef01abc51c639620b7729
SinPedro <143435476+SinPedro@users.noreply.github.com>
2023-09-06 15:01:25 -0300

    Add files via upload

---
+++ trans/transformacao.js
@@ -0,0 +1,8 @@
+const primeiroNumero = prompt("Digite o primeior número:");  
+const primeiro = Number(primeiroNumero)
+const segundoNumero = prompt("Digite o segundo número:");
+const segundo = Number(segundoNumero)
+
+const resultado = (primeiro + segundo);
+
+console.log("O resultado da soma é", resultado);
